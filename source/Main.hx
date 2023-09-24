8d94010b3b5e50e85fe9fc219152783b9f44ae3c
fnfpwp <117086223+pwp114514@users.noreply.github.com>
2023-09-24 19:45:47 +0800

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -1,21 +1,19 @@
 import openfl.display.Sprite;
-import openfl.geom.Circle;
 import openfl.Assets;
 
 class Main extends Sprite {
     public function new() {
         super();
-
-        // 创建圆环形状
-        var ringShape = new Circle(50); // 圆环的半径为50像素
-
-        // 设置圆环颜色
-        ringShape.color = 0xFF0000; // 红色
-
-        // 设置线宽
-        ringShape.thickness = 10; // 线宽为10像素
-
+        
+        var game = {
+		width: 1280, // WINDOW width
+		height: 720, // WINDOW height
+		initialState: PlayState, // initial game state
+		framerate: 120, // default framerate
+		skipSplash: true, // if the default flixel splash screen should be skipped
+		startFullscreen: false // if the game should start at fullscreen mode
+	    };
         // 将圆环形状添加到舞台上
-        addChild(ringShape);
+        addChild(new FlxGame(game.width, game.height, game.initialState, game.framerate, game.framerate, game.skipSplash, game.startFullscreen))
     }
 }
