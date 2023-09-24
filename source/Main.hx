b14ffb5df22eb5d0afd4921e96228a852fab2c74
fnfpwp <117086223+pwp114514@users.noreply.github.com>
2023-09-24 20:01:53 +0800

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -1,5 +1,6 @@
 import openfl.display.Sprite;
 import openfl.Assets;
+import flixel.FlxGame;
 
 class Main extends Sprite {
     public function new() {
@@ -8,12 +9,11 @@ class Main extends Sprite {
         var game = {
 		width: 1280, // WINDOW width
 		height: 720, // WINDOW height
-		initialState: PlayState, // initial game state
 		framerate: 120, // default framerate
 		skipSplash: true, // if the default flixel splash screen should be skipped
 		startFullscreen: false // if the game should start at fullscreen mode
 	    };
         // 将圆环形状添加到舞台上
-        addChild(new FlxGame(game.width, game.height, game.initialState, game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
+        addChild(new FlxGame(game.width, game.height, PlayState, game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
     }
 }
